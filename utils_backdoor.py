8a3b57365ac3af8ded4f61046d2e42e53c45cba8
arppy <berta@inf.u-szeged.hu>
2022-02-02 15:27:25 +0100

    minor debug

--- utils_backdoor.py
+++ utils_backdoor.py
@@ -12,8 +12,8 @@ import numpy as np
 import cv2
 import sys
 sys.path.append("..")
-# from networks.resnet import ResNet18
-from model import ResNet18
+from networks.cnn import CNN
+#from model import ResNet18
 
 def dump_image(x, filename, format):
     #img = image.array_to_img(x, scale=False)
@@ -37,7 +37,7 @@ def load_dataset(data_filename, keys=None):
     return dataset
 
 def load_model(model_file, device):
-    net = ResNet18().to(device)
+    net = CNN().to(device)
     model = torch.load(model_file)
     net.load_state_dict(model.state_dict())
 
